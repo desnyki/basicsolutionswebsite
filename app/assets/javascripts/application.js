ffebfc7b1a7bd26fb4e9a81bc6b9b16cb114c496
aquint <aquinton_22@hotmail.com>
2014-10-16 22:17:16 -0400

    slick

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,13 @@
 //= require turbolinks
 //= require_tree .
 //= require jquery.slick
+
+$(document).ready(function(){
+      $('.slider').slick({
+          //  setting-name: setting-value
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          autoplay: true,
+          autoplaySpeed: 4000,
+        });
+      });
