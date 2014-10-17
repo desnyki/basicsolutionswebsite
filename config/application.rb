87d908fb67debfa6049a7136c3d87ad2504c9145
aquint <aquinton_22@hotmail.com>
2014-10-17 10:52:24 -0400

    add mailer smpt

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,11 @@ module Basic
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    ActionMailer::Base.smtp_settings = {
+    address: 'smtp.mandrillapp.com',
+    port: 587,
+    user_name: ENV['basicsolutionscanada@gmail.com'],
+    password: ENV['mmjiLhkgRGGYfJElazP28A']
+    }
   end
 end
