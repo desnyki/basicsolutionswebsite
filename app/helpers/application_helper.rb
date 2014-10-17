03b67763aa277a9d16670b8e2498b961a4835db6
aquint <aquinton_22@hotmail.com>
2014-10-16 20:31:10 -0400

    add html to views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+	def useSheet(sheet = '')
+		if sheet === 'home'
+			stylesheet_link_tag    "custom", media: "all", "data-turbolinks-track" => true
+		elsif sheet === 'services'
+			stylesheet_link_tag    "services-styles", media: "all", "data-turbolinks-track" => true
+		else sheet === 'contact'
+			stylesheet_link_tag    "contact-styles", media: "all", "data-turbolinks-track" => true
+		end
+	end
 end
